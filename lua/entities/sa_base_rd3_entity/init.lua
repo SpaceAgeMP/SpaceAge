a3838d6bb6b1c7512f5b8e9223e95a8c99aaf70b
Doridian <git@doridian.net>
2024-04-02 16:30:53 -0700

    Actually require faction association to spawn faction-limited entities

--- lua/entities/sa_base_rd3_entity/init.lua
+++ lua/entities/sa_base_rd3_entity/init.lua
@@ -6,11 +6,19 @@ function ENT:CAF_PostInit()
 	self:InitializeRankedVars()
 
 	if self.ForcedModel and self:GetModel():lower() ~= self.ForcedModel:lower() then
+		ply:AddHint("Tried to spawn entity with invalid model (got \"" .. self:GetModel() .. "\", required \""  .. self.ForcedModel .. "\")!", NOTIFY_ERROR, 5)
 		self:Remove()
 		return
 	end
 
-	self:CalcVars(self:GetTable().Founder)
+	local ply = self:GetTable().Founder
+	if self.RequireFaction and ply.sa_data.faction_name ~= self.RequireFaction then
+		local faction = SA.Factions.GetByName(self.RequireFaction)
+		ply:AddHint("You must be a member of " .. faction.display_name .. " to spawn this entity!", NOTIFY_ERROR, 5)
+		self:Remove()
+		return
+	end
+	self:CalcVars(ply)
 end
 
 function ENT:CalcVars(ply)
