12d8628ef74b9717532d5c6b26374ba46f46fe92
Doridian <git@doridian.net>
2025-04-21 23:36:17 -0700

    actually

--- lua/sa/terminal/client/main.lua
+++ lua/sa/terminal/client/main.lua
@@ -509,11 +509,11 @@ local function sa_term_update(_, tbl)
 
 	if SA_CloseButton and SA_PrestigeLevelButton then
 		if lv < 5 or not canReset then
-			SA_CloseButton:SetPos(315, 660)
-			SA_PrestigeLevelButton:SetVisible(true)
-		else
 			SA_CloseButton:SetPos(370, 660)
 			SA_PrestigeLevelButton:SetVisible(false)
+		else
+			SA_CloseButton:SetPos(315, 660)
+			SA_PrestigeLevelButton:SetVisible(true)
 		end
 	end
 
